22a2bcee5dec89a35a496d6eea8837a75e6c008b
Suresh Hothur <hothur11was08@gmail.com>
2025-11-30 13:59:48 +0000

    Fix settings.py defaults for DB_URL and TF_SERVING_URL

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,9 +1,16 @@
+import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    env: str = "dev"
-    db_url: str  # will be injected from Docker Compose (DB_URL)
-    tf_serving_url: str  # will be injected from Docker Compose (TF_SERVING_URL)
+    env: str = os.getenv("ENV", "dev")
+    db_url: str = os.getenv(
+        "DB_URL",
+        "postgresql://credit_user:credit_pass@postgres:5432/credit"
+    )
+    tf_serving_url: str = os.getenv(
+        "TF_SERVING_URL",
+        "http://tfserving:8501/v1/models/credit_model:predict"
+    )
 
     class Config:
         env_file = ".env"
